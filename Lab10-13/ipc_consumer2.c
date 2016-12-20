185d8785bae51b60a877178c9d099b01a6b4d4bc
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-20 23:39:01 +0900

    Lab 11 source study

--- Lab10-13/ipc_consumer2.c
+++ Lab10-13/ipc_consumer2.c
@@ -15,6 +15,7 @@
 char* shm = NULL;
 void shm_free(int sig) {
 	if(shm!=NULL)
+		// shmdt fuction is deattach shared memory in process
 		shmdt(shm);
 }
 int main(int argc, char* argv[]) {
@@ -22,21 +23,26 @@ int main(int argc, char* argv[]) {
 	char myPid[16];	
 	sprintf(myPid, "%d", getpid());
 	char producerPid[MYSHM_SIZE], studentId[MYSHM_SIZE];
+	// shmget function is create shared memory
+	// 0666 | IPC_CREATE is option, No shared memory depending on key create,
+	// but exist then ignore and set file acess mode
+	// return != -1 is success , -1 is fail
 	int shmid = shmget(MYSHM_KEY, MYSHM_SIZE, 0666 | IPC_CREAT);
 	if(shmid==-1) {
 		perror("shmget failed\n");
 		exit(EXIT_FAILURE);
 	}
+	// shmat function is attach shared memory in process
 	shm = shmat(shmid, NULL, 0);
 	if(shm == (char*)(-1)) {
 		perror("shmat failed\n");
 		exit(EXIT_FAILURE);
 	}
-	(void) signal(SIGTERM, shm_free);
-	(void) signal(SIGKILL, shm_free);
-	(void) signal(SIGSTOP, shm_free);
+	(void) signal(SIGTERM, shm_free); // reset(?)
+	(void) signal(SIGKILL, shm_free); 
+	(void) signal(SIGSTOP, shm_free); 
 	while(1) {
-		// read 1.myPid 2.studentId 
+		// read ipc_producer2 pid, student id in shared memory
 		while(shm[0]!=PRODUCER_WRITED)
 			sleep(0);
 		strncpy(producerPid, shm+1, MYSHM_SIZE-1);
@@ -48,7 +54,7 @@ int main(int argc, char* argv[]) {
 		printf("producer_pid:%s, student_id:%s\n",
 				producerPid, studentId);
 
-		// write 1.consumerPid 2.studentName
+		// write ipc_consumer2 pid, student name in shared memory
 		strcpy(shm+1, myPid);
 		shm[0] = CONSUMER_WRITED;
 		while(shm[0]!=ANYONE_WRITED)
