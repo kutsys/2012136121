83f6762a13d10820f112adc63af076ba0c8b01bc
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-21 00:33:20 +0900

    Lab14 source study

--- Lab10-13/ipc_consumer4.c
+++ Lab10-13/ipc_consumer4.c
@@ -15,7 +15,9 @@ int main(int argc, char* argv[]) {
 	sprintf(myPid, "%d", getpid());
 	char producerPid[MYSOCK_BUF_SIZE], studentId[MYSOCK_BUF_SIZE];
 	char buf[MYSOCK_BUF_SIZE];
-
+	// socket function is create socket ans retrun socket
+	// 1st variable is trasmission setting, AF_UNIX is process transmission in same system 
+	// 2nd variable is data transmassion type, SOCK_STREAM is using TCP/IP
 	int mySocket = socket(AF_UNIX, SOCK_STREAM, 0);
 	struct sockaddr_un serverAddress;
 	serverAddress.sun_family = AF_UNIX;
@@ -23,24 +25,28 @@ int main(int argc, char* argv[]) {
 	socklen_t addressLen = sizeof(serverAddress);
 
 	while(1) {
+		// connect function is request connection to server
+		// and succuss retrun 0;
 		int result = connect(mySocket,
 				(struct sockaddr*)&serverAddress, addressLen);
 		if(result!=0) {
 			sleep(0);
 			continue;
 		}
-		// read 1.producerPid 2.studentId 
+		// read function read ipc_producer4 pid, strudnet id in socket
 		read(mySocket, producerPid, MYSOCK_BUF_SIZE);
 		read(mySocket, studentId, MYSOCK_BUF_SIZE);
 		printf("producer_pid:%s, student_id:%s\n",
 				producerPid, studentId);
 
-		// write 1.myPid 2.studentName
+		// write function wirte ipc_consumer4 pid student name in socket
 		strcpy(buf, myPid);
 		write(mySocket, buf, MYSOCK_BUF_SIZE);
 		strcpy(buf, studentName);
 		write(mySocket, buf, MYSOCK_BUF_SIZE);
+		// close socket
 		close(mySocket);
+		// create socket
 		mySocket = socket(AF_UNIX, SOCK_STREAM, 0);
 	}
 	exit(EXIT_FAILURE);
